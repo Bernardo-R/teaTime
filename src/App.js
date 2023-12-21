6d6b9f12bd12785126285eab18793f4196ced64e
Michelle Okagbue <umbralcompendium@gmail.com>
2023-12-21 01:42:59 -0500

    file organization, routing hook up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import React from "react";
+// import { } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import StoreFront from "./components/StoreFront";
+import About from "./components/Pages/About";
+import Home from "./components/Pages/Home";
 
 function App() {
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 border py-4">
+    <>
       <Navbar />
-      <StoreFront />
+      <Router>
+        {/* <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 border py-4"> */}
+        <Routes>
+          <Route  path="/home" element={<Home/>}></Route>
+          <Route path="/" element={<StoreFront/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+        </Routes>
+        {/* </div> */}
+      </Router>
       <Footer />
-    </div>
+    </>
   );
 }
 
