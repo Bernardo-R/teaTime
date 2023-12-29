b23739a6e84cede0572191bbc51053cce493384d
Bernardo-R <brosario12@live.com>
2023-12-29 14:02:21 -0500

    create a composable to reuse api calls on any component

---
+++ composable/useApi.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useApi = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useApi;
