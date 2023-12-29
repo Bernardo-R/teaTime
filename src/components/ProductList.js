186ce87d8728c9967550e6da70d24fb00276f8cc
Bernardo-R <brosario12@live.com>
2023-12-29 14:54:45 -0500

    updating ProductList Component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,26 @@
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  // More products...
-];
+import React from "react";
+import useApi from "../composable/useApi";
 
-export default function Example() {
+const FetchData = () => {
+  const apiUrl = "https://boonakitea.cyclic.app/api/all";
+  const { data, loading, error } = useApi(apiUrl);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong: {error.message}</p>;
+
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: "Basic Tee",
+  //     href: "#",
+  //     imageSrc:
+  //       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+  //     imageAlt: "Front of men's Basic Tee in black.",
+  //     price: "$35",
+  //     color: "Black",
+  //   },
+  //   // More products...
+  // ];
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -21,27 +29,27 @@ export default function Example() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
+          {data.map((product) => (
+            <div key={product._id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
+                  src={product.image}
+                  alt={product.altnames}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                    <a href={product.sources}>
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
                     </a>
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                  <p className="mt-1 text-sm text-gray-500">{product.type}</p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">
-                  {product.price}
+                  {product.caffeine}
                 </p>
               </div>
             </div>
@@ -50,4 +58,6 @@ export default function Example() {
       </div>
     </div>
   );
-}
+};
+
+export default FetchData;
