95732a41992f573b39fc6542d8cbef610804bd36
Cmendence <chris.mendence@gmail.com>
2024-01-05 22:11:29 -0500

    testing

--- src/components/Footer/Warranty.js
+++ src/components/Footer/Warranty.js
@@ -2,28 +2,6 @@ import React from "react";
 
 export default function Warranty() {
   return (
-    // <div>
-    //    <p>
-    //       Ah, the sacred art of tea-surance! Welcome to the Tea Time Warranty Wonderland, where we promise to stand by our
-    //       products like a loyal tea cozy clings to a teapot in a winter storm. Our warranty, or as we like to call it, the
-    //       "Steep Assurance Plan," ensures that your tea-related gadgets are as reliable as your go-to Earl Grey blend.
-    //    </p>
-    //    <p>
-    //       In the unlikely event that your Tea Time teapot starts singing "I'm a Little Teapot" in a tone-deaf manner or your tea infuser
-    //       develops a rebellious streak and decides to explore life beyond the kitchen sink, fear not! Our warranty covers such eccentricities.
-    //       We understand that sometimes tea accessories can be a bit temperamental, but worry not – we've got your steeping back.
-    //    </p>
-    //    <p>
-    //       Should you encounter any issues, simply send a raven, carrier pigeon, or, you know, an email to our Tea-surance Wizards.
-    //       They'll work their magic and have your teapot back in harmony or your infuser back to obedient steeping duty in no time.
-    //       Just remember, this warranty doesn't cover accidental tea spills caused by overly enthusiastic dance moves during tea parties.
-    //       We believe in dancing like nobody's watching, but tea like somebody's carpet is.
-    //    </p>
-    //    <p>
-    //       So, enjoy your Tea Time products with the peace of mind that only comes from knowing we've got the kettle on the boil for you.
-    //       Happy sipping and may your tea always be hot and your warranties evergreen!
-    //    </p>
-    // </div>
     <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
       <div className="absolute inset-0 -z-10 overflow-hidden">
         <svg
