6bdd1d8b830be97a5c37f23173e4ab5531ebe1e9
Bernardo-R <brosario12@live.com>
2024-01-05 21:21:51 -0500

    adding heading section  as comment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,162 @@
-import React from 'react'
+import React from "react";
 
-export default function Contact(){
-   return(
-      <div>
+export default function Contact() {
+  return (
+    <div>
       <h1>Contact Tea Time</h1>
 
       <h2>Tea-rrific Inquiries:</h2>
-         <p>
-            For all your tea-related musings, inquiries, and delightful revelations, send a raven, carrier pigeon, 
-            or a modern electronic missive to our Tea Time Telegraphers at 
-            <a href="mailto:teatime@brewfullofjoy.com">teatime@brewfullofjoy.com</a>. They're equipped 
-            with tealepathic communication skills and are eager to assist you on your tea journey.
-         </p>
+      <p>
+        For all your tea-related musings, inquiries, and delightful revelations,
+        send a raven, carrier pigeon, or a modern electronic missive to our Tea
+        Time Telegraphers at
+        <a href="mailto:teatime@brewfullofjoy.com">teatime@brewfullofjoy.com</a>
+        . They're equipped with tealepathic communication skills and are eager
+        to assist you on your tea journey.
+      </p>
 
       <h2>Tea-surance Wizards Hotline:</h2>
-         <p>
-            Should you find yourself in need of tea-surance guidance, call upon our Tea-surance Wizards by dialing 
-            <a href="tel:1800TEAWIZZ">1-800-TEA-WIZZ</a>. They specialize in mending the mystical bonds between tea and tea drinker, 
-            ensuring your tea time is as smooth as a well-steeped Darjeeling.
-         </p>
+      <p>
+        Should you find yourself in need of tea-surance guidance, call upon our
+        Tea-surance Wizards by dialing
+        <a href="tel:1800TEAWIZZ">1-800-TEA-WIZZ</a>. They specialize in mending
+        the mystical bonds between tea and tea drinker, ensuring your tea time
+        is as smooth as a well-steeped Darjeeling.
+      </p>
       <h2>Tea Time HQ - Virtual Tea Garden:</h2>
-         <p>
-            Visit our imaginary headquarters in the Virtual Tea Garden, nestled between the Enchanted Earl Grey Forest 
-            and the Matcha Mountains. While physically unreachable, it's the perfect place for daydreaming about tea.
-         </p>
-
-      </div>
-   )
+      <p>
+        Visit our imaginary headquarters in the Virtual Tea Garden, nestled
+        between the Enchanted Earl Grey Forest and the Matcha Mountains. While
+        physically unreachable, it's the perfect place for daydreaming about
+        tea.
+      </p>
+    </div>
+  );
 }
+
+// export default function Example() {
+//    return (
+//      <div className="bg-white py-24 sm:py-32">
+//        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+//          <div className="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
+//            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+//              <div>
+//                <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+//                <p className="mt-4 leading-7 text-gray-600">
+//                  Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac pellentesque.
+//                </p>
+//              </div>
+//              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Collaborate</h3>
+//                  <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+//                    <div>
+//                      <dt className="sr-only">Email</dt>
+//                      <dd>
+//                        <a className="font-semibold text-indigo-600" href="mailto:collaborate@example.com">
+//                          collaborate@example.com
+//                        </a>
+//                      </dd>
+//                    </div>
+//                    <div className="mt-1">
+//                      <dt className="sr-only">Phone number</dt>
+//                      <dd>+1 (555) 905-2345</dd>
+//                    </div>
+//                  </dl>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Press</h3>
+//                  <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+//                    <div>
+//                      <dt className="sr-only">Email</dt>
+//                      <dd>
+//                        <a className="font-semibold text-indigo-600" href="mailto:press@example.com">
+//                          press@example.com
+//                        </a>
+//                      </dd>
+//                    </div>
+//                    <div className="mt-1">
+//                      <dt className="sr-only">Phone number</dt>
+//                      <dd>+1 (555) 905-3456</dd>
+//                    </div>
+//                  </dl>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Join our team</h3>
+//                  <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+//                    <div>
+//                      <dt className="sr-only">Email</dt>
+//                      <dd>
+//                        <a className="font-semibold text-indigo-600" href="mailto:careers@example.com">
+//                          careers@example.com
+//                        </a>
+//                      </dd>
+//                    </div>
+//                    <div className="mt-1">
+//                      <dt className="sr-only">Phone number</dt>
+//                      <dd>+1 (555) 905-4567</dd>
+//                    </div>
+//                  </dl>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Say hello</h3>
+//                  <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+//                    <div>
+//                      <dt className="sr-only">Email</dt>
+//                      <dd>
+//                        <a className="font-semibold text-indigo-600" href="mailto:hello@example.com">
+//                          hello@example.com
+//                        </a>
+//                      </dd>
+//                    </div>
+//                    <div className="mt-1">
+//                      <dt className="sr-only">Phone number</dt>
+//                      <dd>+1 (555) 905-5678</dd>
+//                    </div>
+//                  </dl>
+//                </div>
+//              </div>
+//            </div>
+//            <div className="grid grid-cols-1 gap-x-8 gap-y-10 pt-16 lg:grid-cols-3">
+//              <div>
+//                <h2 className="text-3xl font-bold tracking-tight text-gray-900">Locations</h2>
+//                <p className="mt-4 leading-7 text-gray-600">
+//                  Consequat sunt cillum cillum elit sint. Qui occaecat nisi in ipsum commodo.
+//                </p>
+//              </div>
+//              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Los Angeles</h3>
+//                  <address className="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+//                    <p>4556 Brendan Ferry</p>
+//                    <p>Los Angeles, CA 90210</p>
+//                  </address>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">New York</h3>
+//                  <address className="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+//                    <p>886 Walter Street</p>
+//                    <p>New York, NY 12345</p>
+//                  </address>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Toronto</h3>
+//                  <address className="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+//                    <p>7363 Cynthia Pass</p>
+//                    <p>Toronto, ON N3Y 4H8</p>
+//                  </address>
+//                </div>
+//                <div className="rounded-2xl bg-gray-50 p-10">
+//                  <h3 className="text-base font-semibold leading-7 text-gray-900">Chicago</h3>
+//                  <address className="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+//                    <p>726 Mavis Island</p>
+//                    <p>Chicago, IL 60601</p>
+//                  </address>
+//                </div>
+//              </div>
+//            </div>
+//          </div>
+//        </div>
+//      </div>
+//    )
+//  }
