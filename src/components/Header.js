a8a6f2fba2ad0186d7027ac93ea352d0db985ad1
Michelle Okagbue <umbralcompendium@gmail.com>
2024-02-02 17:29:28 -0500

    header added to storefront

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-export default function Example() {
+import HeaderImg from "../assets/imgs/storefrontImages/yousef-espanioly-hbQeCpo-Cp0-unsplash (1).jpg";
+
+export default function Header() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <div >
+       <div className="relative isolate overflow-hidden bg-gray-900 py-15 sm:py-20">
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        src={HeaderImg}
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
@@ -11,7 +14,8 @@ export default function Example() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          // className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#090909] to-[#c26100] opacity-50"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -23,7 +27,7 @@ export default function Example() {
         aria-hidden="true"
       >
         <div
-          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#090909] to-[#c26100]  opacity-50"
           style={{
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -31,17 +35,17 @@ export default function Example() {
         />
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Support center
+        <div className="mx-auto max-w-2xl lg:mx-0 mt-3">
+          <h2 className="font-bold tracking-tight text-white title text-7xl md:text-9xl ">
+            Tea Time
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-300">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+          <p className="my-3 text-base md:text-lg leading-8 text-gray-100" >
+            The Art of Tea is not just a Ritual, but a Journey
           </p>
         </div>
       </div>
+    </div>
+    <div className="bg-lime-800 h-10 w-100"></div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -425,18 +425,6 @@ const Navbar = ({ searchQuery, setSearchQuery }) => {
                     <span className="sr-only">Open menu</span>
                     <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                   </button>
-
-                  {/* Search */}
-                  {/* <a
-                    href="https://www.google.com/"
-                    className="ml-2 p-2 text-gray-400 hover:text-gray-500"
-                  >
-                    <span className="sr-only">Search</span>
-                    <MagnifyingGlassIcon
-                      className="h-6 w-6"
-                      aria-hidden="true"
-                    />
-                  </a> */}
                 </div>
 
                 {/* Logo (lg-) */}
@@ -447,40 +435,7 @@ const Navbar = ({ searchQuery, setSearchQuery }) => {
                 </a>
 
                 <div className="flex flex-1 items-center justify-end">
-                  {/* <input 
-                     type="text" 
-                     placeholder="Search Our Selection" 
-                     className="p-1 border border-gray-300 rounded-lg text-sm m-2
-                    focus:border-lime-700 focus:border-2 focus:ring-0 font-semibold text-gray-600"
-                     name="search"
-                     value={searchQuery}
-                     onChange={e => setSearchQuery(e.target.value)}
-                     onKeyPress={e => {
-                        if (e.key === 'Enter') {
-                          handleSearch();
-                        }
-                      }}
-                  />
-                  <button
-                     onClick={handleSearch}
-                    className="hidden text-sm font-medium text-gray-700 hover:text-lime-600 lg:block border-lime-700"
-                    type="button"
-                  >
-                    Search
-                  </button> */}
-
                   <div className="flex items-center lg:ml-8">
-                    {/* Help */}
-                    {/* <a
-                      href="https://www.google.com/"
-                      className="p-2 text-gray-400 hover:text-gray-500 lg:hidden"
-                    >
-                      <span className="sr-only">Help</span>
-                      <QuestionMarkCircleIcon
-                        className="h-6 w-6"
-                        aria-hidden="true"
-                      />
-                    </a> */}
                     <Link
                       to="/contact"
                       className="hidden text-sm font-medium text-gray-700 hover:text-lime-600 lg:block"
